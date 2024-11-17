dc605e44c11af68a214872247503a306b6699344
Shayan1503 <shayandasgupta1502@gmail.com>
2024-11-18 00:02:53 +0530

    minor change

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	},
 	preprocess: vitePreprocess()
 };
